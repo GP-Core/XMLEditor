ab67bf3e55346f6adb8a60cc7972a9fbaaa841e4
Gp-Core <fabian18102005@gmail.com>
2025-03-28 01:44:52 -0600

    28/03/2025- Cambios realizados hasta el momento, donde se utiliza 2 hilos para abrir un archivo csv para despues convertirlo a xml. Con un faltante posible de mejora.

---
+++ XMLEditor/frmCompleto.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace XMLEditor
+{
+    partial class frmCompleto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dgvMain = new System.Windows.Forms.DataGridView();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.dgvMain, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnConvertir, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btnAbrir, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.05141F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 79.94859F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(803, 444);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // dgvMain
+            // 
+            this.dgvMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvMain.Location = new System.Drawing.Point(3, 78);
+            this.dgvMain.Name = "dgvMain";
+            this.dgvMain.RowHeadersWidth = 51;
+            this.dgvMain.RowTemplate.Height = 24;
+            this.dgvMain.Size = new System.Drawing.Size(797, 294);
+            this.dgvMain.TabIndex = 2;
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConvertir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(159)))), ((int)(((byte)(128)))));
+            this.btnConvertir.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvertir.Location = new System.Drawing.Point(3, 378);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(797, 30);
+            this.btnConvertir.TabIndex = 1;
+            this.btnConvertir.Text = "CONVERTIR A XML";
+            this.btnConvertir.UseVisualStyleBackColor = false;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAbrir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(159)))), ((int)(((byte)(128)))));
+            this.btnAbrir.Font = new System.Drawing.Font("Comic Sans MS", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAbrir.Location = new System.Drawing.Point(3, 414);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(797, 27);
+            this.btnAbrir.TabIndex = 0;
+            this.btnAbrir.Text = "ABRIR CSV";
+            this.btnAbrir.UseVisualStyleBackColor = false;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(134)))), ((int)(((byte)(106)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(797, 69);
+            this.panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(202, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(363, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Archivo CSV- XML";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "ARCHIVO CSV|*.csv";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "ARCHIVO XML|*.xml";
+            // 
+            // frmCompleto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(70)))), ((int)(((byte)(82)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "frmCompleto";
+            this.Text = "Archivo CSV- XML";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmCompleto_FormClosed);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.DataGridView dgvMain;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
---
+++ XMLEditor/frmCompleto.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace XMLEditor
+{
+    public partial class frmCompleto : Form
+    {
+        public frmCompleto()
+        {
+            InitializeComponent();
+        }
+        string titulo;
+        XElement[] personass;
+        Thread h1,hpers ;
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult res = openFileDialog1.ShowDialog();
+                if (res == DialogResult.OK)
+                {
+                    titulo = openFileDialog1.FileName;
+                    string texto = File.ReadAllText(titulo);
+
+                    string[] reg = texto.Split('\n');
+                    string[] col = reg[0].Split(',');
+                    dgvMain.ColumnCount = col.Length;
+                    dgvMain.RowCount = reg.Length;
+
+                    for (int i = 0; i < reg.Length -1; i++)
+                    {
+                        string[] textoR = reg[i].Split(',');
+
+                        for (int j = 0; j < col.Length; j++)
+                        {
+                            dgvMain.Rows[i].Cells[j].Value = textoR[j];
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error inesperado al abrir el archivo: " + ex);
+            }
+        }
+
+        private void hiloMostrarDatos()
+        {
+            
+        }
+
+        private void frmCompleto_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            h1.Abort();
+            hpers.Abort();
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            DialogResult res = saveFileDialog1.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                titulo = saveFileDialog1.FileName;
+            }
+            hpers = new Thread(new ThreadStart(personas));
+            hpers.Start();
+            hpers.Join();
+            h1 = new Thread(new ThreadStart(hilo1));
+            h1.Start();
+        }
+        private void hilo1()
+        {
+            XDocument xmlDoc = new XDocument(new XElement("Personas"));
+            xmlDoc.Root.Add(personass);
+          
+
+            xmlDoc.Save(titulo);
+            MessageBox.Show("¡Guardado con exito!");
+
+        }
+
+       private  void personas()
+        {
+             personass = new XElement[dgvMain.RowCount];
+            for (int j =1; j<dgvMain.RowCount-1;j++) {
+                personass[j] = new XElement("Persona",
+                               new XAttribute("ID",j+1),atributos(j));
+            }
+
+        }
+
+        private XElement[] atributos(int indice ) 
+        {
+            int i = 0;
+            XElement[] atributos = new XElement[dgvMain.ColumnCount];
+            while (i < dgvMain.ColumnCount) {
+                string nombrec = dgvMain[i, 0].Value.ToString().Contains("\r")? dgvMain[i, 0].Value.ToString().Replace("\r",""): dgvMain[i, 0].Value.ToString();
+                atributos[i] = new XElement(nombrec, dgvMain[i, indice].Value.ToString());
+                i++;
+            }
+            return atributos;
+            
+        }
+    }
+}
