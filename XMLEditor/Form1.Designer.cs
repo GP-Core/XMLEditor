7f422be06ae14a87288a49ec318421d6cd004974
Gp-Core <fabian18102005@gmail.com>
2025-03-27 21:58:59 -0600

    Agregar archivos de proyecto.

---
+++ XMLEditor/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace XMLEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvXML = new System.Windows.Forms.DataGridView();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.aPaterno = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.aMaterno = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.edad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvXML)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvXML
+            // 
+            this.dgvXML.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvXML.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nombre,
+            this.aPaterno,
+            this.aMaterno,
+            this.edad});
+            this.dgvXML.Location = new System.Drawing.Point(33, 147);
+            this.dgvXML.Name = "dgvXML";
+            this.dgvXML.RowHeadersWidth = 51;
+            this.dgvXML.RowTemplate.Height = 24;
+            this.dgvXML.Size = new System.Drawing.Size(719, 263);
+            this.dgvXML.TabIndex = 0;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(153)))), ((int)(((byte)(174)))));
+            this.btnGuardar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnGuardar.Location = new System.Drawing.Point(274, 416);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(225, 55);
+            this.btnGuardar.TabIndex = 1;
+            this.btnGuardar.Text = "GUARDAR";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // nombre
+            // 
+            this.nombre.HeaderText = "NOMBRE";
+            this.nombre.MinimumWidth = 6;
+            this.nombre.Name = "nombre";
+            this.nombre.Width = 180;
+            // 
+            // aPaterno
+            // 
+            this.aPaterno.HeaderText = "A PATERNO";
+            this.aPaterno.MinimumWidth = 6;
+            this.aPaterno.Name = "aPaterno";
+            this.aPaterno.Width = 180;
+            // 
+            // aMaterno
+            // 
+            this.aMaterno.HeaderText = "A MATERNO";
+            this.aMaterno.MinimumWidth = 6;
+            this.aMaterno.Name = "aMaterno";
+            this.aMaterno.Width = 180;
+            // 
+            // edad
+            // 
+            this.edad.HeaderText = "EDAD";
+            this.edad.MinimumWidth = 6;
+            this.edad.Name = "edad";
+            this.edad.Width = 125;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(109)))), ((int)(((byte)(130)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(33, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(719, 100);
+            this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 22.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(51)))), ((int)(((byte)(130)))));
+            this.label1.Location = new System.Drawing.Point(216, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(270, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Archivo XLML";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "|*.xml";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(236)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(800, 502);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.dgvXML);
+            this.Name = "Form1";
+            this.Text = "DataGrid a XLML";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvXML)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvXML;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn aPaterno;
+        private System.Windows.Forms.DataGridViewTextBoxColumn aMaterno;
+        private System.Windows.Forms.DataGridViewTextBoxColumn edad;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ XMLEditor/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace XMLEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Thread p1;
+        string titulo;
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            DialogResult res = saveFileDialog1.ShowDialog();
+            if (res == DialogResult.OK) { 
+                titulo = saveFileDialog1.FileName;
+            }
+            p1 = new Thread(new ThreadStart(hilo1));
+            p1.Start();
+        }
+
+        private void hilo1()
+        {
+            XDocument xmlDoc = new XDocument(new XElement("Personas"));
+
+            int i = 0;
+            while (dgvXML[0, i].Value != null && dgvXML[0, i].Value.ToString() != "")
+            {
+                xmlDoc.Root.Add(
+                new XElement("Persona",
+                    new XAttribute("ID", i + 1),
+                    new XElement("Nombre", dgvXML[0, i].Value.ToString()),
+                    new XElement("Paterno", dgvXML[1, i].Value.ToString()),
+                    new XElement("Materno", dgvXML[2, i].Value.ToString()),
+                    new XElement("Edad", dgvXML[3, i].Value.ToString())
+                   ));
+                i++;
+            }
+     
+            xmlDoc.Save(titulo);
+            MessageBox.Show("¡Guardado con exito!");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            p1.Abort();
+        }
+    }
+
+
+
+    
+}
